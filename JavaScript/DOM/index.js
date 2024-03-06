f3a5bc66fa960664b24c2fb5647e4630c54f4917
AndreiBalasca <balasca.andrei@gmail.com>
2024-03-06 03:53:43 -1000

    used getelementbyX and queryselector

--- JavaScript/DOM/index.js
+++ JavaScript/DOM/index.js
@@ -1,1 +1,6 @@
-document.query
+document.firstElementChild.lastElementChild.querySelector(
+  "ul"
+).lastElementChild.innerHTML = "Andrei";
+
+document.getElementsByTagName("li")[2].style.color = "purple";
+document.querySelector("#list a").style.color = "red";
