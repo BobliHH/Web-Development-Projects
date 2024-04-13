ba3aa7cff643592f6c9fc677f07b9eac570e3ace
AndreiBalasca <46415669+BobliHH@users.noreply.github.com>
2024-04-13 12:29:59 +0300

    learned some more methods of jQuery

--- JavaScript/JQuery/index.js
+++ JavaScript/JQuery/index.js
@@ -17,3 +17,13 @@ $("body").keydown(function (event) {
 $("h1").on("mouseover", function () {
   $(this).css("color", "purple");
 });
+$("h1").before("<button>New</button>");
+$("h1").after("<button>New</button>");
+$("h1").prepend("<button>New</button>");
+$("h1").append("<button>New</button>");
+
+$("button").on("click", function () {
+  $("h1").slideUp().slideDown().animate({
+    opacity: 0.5,
+  });
+});
