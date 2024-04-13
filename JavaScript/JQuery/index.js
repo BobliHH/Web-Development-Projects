4496c8cf4528edb4a1d7e2638f3d23286c0a8ce1
AndreiBalasca <46415669+BobliHH@users.noreply.github.com>
2024-04-13 12:11:09 +0300

    learning to use jQuery

--- JavaScript/JQuery/index.js
+++ JavaScript/JQuery/index.js
@@ -2,5 +2,18 @@ $("h1").css("color", "red");
 $("button").css("background-color", "yellow");
 
 $("button").click(function () {
-  $(this).css("background-color", "red");
+  $("h1").css("color", "red");
+  $(this).css("background-color", "purple");
+});
+$("h1").click(function () {
+  $(this).css("color", "purple");
+});
+$("input").keydown(function (event) {
+  $("h1").text(event.key);
+});
+$("body").keydown(function (event) {
+  $("h1").text(event.key);
+});
+$("h1").on("mouseover", function () {
+  $(this).css("color", "purple");
 });
