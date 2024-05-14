88fd9c890e78fd761a7ace0fc278030ebb87f634
AndreiBalasca <46415669+BobliHH@users.noreply.github.com>
2024-05-14 14:12:01 +0300

    resolved superhero task also I think solution is deprecated

--- Node/2.3 NPM/index.js
+++ Node/2.3 NPM/index.js
@@ -1,4 +1,12 @@
-let generateName = require("sillyname");
-let sillyname = generateName();
+// let generateName = require("sillyname");
+import generateName from "sillyname";
+import superheroes, { randomSuperhero } from "superheroes";
 
+let superHeroName = randomSuperhero();
+// let superHeroName1 = superheroes.random();
+superheroes;
+console.log(superheroes);
+let sillyname = generateName();
 console.log(`My name is ${sillyname}.`);
+console.log(`My SuperName is ${superHeroName}!`);
+// console.log(`AKA ${superHeroName1}!`);
